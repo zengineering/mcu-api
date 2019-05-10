deeed4a42ff011a81b8308879bf8cd240aaa29cf
zane <zane@localhost>
2019-05-10 16:36:59 -0600

    improvement: privatize schema.log

--- mcuapi/schema.py
+++ mcuapi/schema.py
@@ -7,22 +7,22 @@ from .utils import set_content_type
 class FilmSchema():
     def __init__(self, db):
         self._db = db
-        self.log = logging.getLogger(__name__)
+        self._log = logging.getLogger(__name__)
 
 
     @falcon.after(set_content_type)
     def on_get(self, req, resp):
         resp.data = msgpack.dumps(self._db.film_schema, use_bin_type=True)
-        self.log.debug('GET character schema')
+        self._log.debug('GET character schema')
 
 
 class CharacterSchema():
     def __init__(self, db):
         self._db = db
-        self.log = logging.getLogger(__name__)
+        self._log = logging.getLogger(__name__)
 
     @falcon.after(set_content_type)
     def on_get(self, req, resp):
         resp.data = msgpack.dumps(self._db.character_schema, use_bin_type=True)
-        self.log.debug('GET character schema')
+        self._log.debug('GET character schema')
 
